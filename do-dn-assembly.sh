8392e8c56cf0078bfcaf09d90b610d3ec4738904
C. Titus Brown <titus@idyll.org>
2013-06-28 03:23:40 +0000

    corrected velvet command for fastq

--- do-dn-assembly.sh
+++ do-dn-assembly.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 for k in {21..51..2}
 do
-echo "velveth dn.$k $k -shortPaired combined-trim.fq.pe.keep.abundfilt.pe.keep -short combined-trim.fq.pe.keep.abundfilt.se.keep combined-trim.fq.se.keep.abundfilt.keep s1_se.filt.keep.abundfilt.keep && velvetg dn.$k -exp_cov auto"
+echo "velveth dn.$k $k -shortPaired combined-trim.fq.pe.keep.abundfilt.pe.keep -short combined-trim.fq.pe.keep.abundfilt.se.keep combined-trim.fq.se.keep.abundfilt.keep -fastq s1_se.filt.keep.abundfilt.keep && velvetg dn.$k -exp_cov auto"
 done
